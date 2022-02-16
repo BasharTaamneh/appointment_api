e26d47385067e0249906d6fe40daadd33158f1c8
bashar taamneh <bashartaamneh55@gmail.com>
2022-02-16 17:05:05 +0200

    edit errors handler

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -3,23 +3,27 @@
 function errorHandler(err, req, res, next) {
     if (typeof err === "string") {
         // custom application errors
-        return res.status(400).json({ message: err });
+        return res.status(400).send({ message: err });
     }
 
     if (err.name === "ValidationError") {
         // mongoose validation errors
-        return res.status(400).json({ message: err.message });
+        return res.status(400).send({ message: err.message });
     }
 
     if (err.name === "UnauthorizedError") {
         // jwt authentication errors
-        return res.status(401).json({ message: "Token not valid" });
+        return res.status(401).send({ message: "Token not valid" });
     }
 
     // default to 400 server errors
-    return res.status(400).json({ message: err.message });
+    return res.status(400).send({ message: err.message });
 }
 
+// .send({
+//     message: "success",
+//     data: result,
+// });
 module.exports = {
     errorHandler,
 };
